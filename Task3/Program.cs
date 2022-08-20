3fa814ff7214ddcbd41f201152a218c256cfbae5
andrey <andreyz3000@ya.ru>
2022-08-20 19:16:11 +0300

    Добавил задание3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//3 дз
+int a;                             
+Console.Write("введите число: ");
+a = Console.Read();
+if (a % 2==1) //формула определение четности числа Если ... и т.д.
+    {
+    Console.WriteLine("число нечетное");
+    }
+    else
+    {
+    Console.WriteLine("Число четное");
+    }
