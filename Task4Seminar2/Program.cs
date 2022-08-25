2c3537bc745c7b663a23949155a681c12e164095
andrey <andreyz3000@ya.ru>
2022-08-25 21:50:10 +0300

    поправил

--- Task4Seminar2/Program.cs
+++ Task4Seminar2/Program.cs
@@ -5,4 +5,4 @@ string Result = " "; //строковая переменная result
 if (number % a == 0 && number % b == 0) Result = "кратно обоим числам"; // проверяем если при деление на число из random переменная a и переменная b равна 0
 else if (number % a != 0 && number % b != 0) Result = "не кратно ни одному из чисел"; //если не равно
 System.Console.WriteLine();
-System.Console.WriteLine($"Число кратное {number} {Result} "); //печатаем на экран
+System.Console.WriteLine($"Число {number} {Result} "); //печатаем на экран
