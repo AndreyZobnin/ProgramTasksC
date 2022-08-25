ad855955881c24d8023b116641c87d45f7927715
andrey <andreyz3000@ya.ru>
2022-08-25 20:05:30 +0300

    поправил дз 7 и 23

--- Task4Seminar2/Program.cs
+++ Task4Seminar2/Program.cs
@@ -1,8 +1,8 @@
-﻿int number = new Random().Next(1, 200); //генератор случайных чисел от 1 до 200
+﻿int number = new Random().Next(1, 1000000); //генератор случайных чисел от 1 до 200
 int a = 7; // переменная а = 7
 int b = 23; 
 string Result = " "; //строковая переменная result
 if (number % a == 0 && number % b == 0) Result = "кратно обоим числам"; // проверяем если при деление на число из random переменная a и переменная b равна 0
 else if (number % a != 0 && number % b != 0) Result = "не кратно ни одному из чисел"; //если не равно
 System.Console.WriteLine();
-System.Console.WriteLine($"Число {number} {Result} "); //печатаем на экран
+System.Console.WriteLine($"Число кратное {number} {Result} "); //печатаем на экран
