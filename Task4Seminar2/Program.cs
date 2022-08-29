5bc0d199dbf4b120c378ee0f7ae8047391401343
andrey <andreyz3000@ya.ru>
2022-08-29 20:16:13 +0300

    .

--- Task4Seminar2/Program.cs
+++ Task4Seminar2/Program.cs
@@ -1,8 +1,17 @@
-﻿int number = new Random().Next(1, 1000000); //генератор случайных чисел от 1 до 200
-int a = 7; // переменная а = 7
-int b = 23; 
-string Result = " "; //строковая переменная result
-if (number % a == 0 && number % b == 0) Result = "кратно обоим числам"; // проверяем если при деление на число из random переменная a и переменная b равна 0
-else if (number % a != 0 && number % b != 0) Result = "не кратно ни одному из чисел"; //если не равно
-System.Console.WriteLine();
-System.Console.WriteLine($"Число {number} {Result} "); //печатаем на экран
+﻿int N = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int [N];
+
+int i = 0;
+int square = 0;
+while ( i < N)
+{
+    array[i] = (i + 1) * (i + 1);
+    square = array[i] * array[i];
+    Console.Write(square + "");
+
+    //array[i] = (i + 1) * (i + 1);
+ 
+    i++;    
+}
+
