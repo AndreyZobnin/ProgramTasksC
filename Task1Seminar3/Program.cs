c56809adb6d1e3051995fec6a7272bb12b0a167c
andrey <andreyz3000@ya.ru>
2022-08-29 20:21:55 +0300

    ДЗ 1 семинар3

---
+++ Task1Seminar3/Program.cs
@@ -0,0 +1,19 @@
+﻿System.Console.WriteLine();
+System.Console.Write("Введите целое число:   ");
+string number = Console.ReadLine();
+int length = number.Length;
+string txtToCheck = number.Replace(" ","");  /// для проверки текста на палиндром
+
+bool IsPalindrome()
+{
+    for (int i = 0; i < length / 2; i++)
+        while (number[i] == number[length - 1])
+        {
+            return true;
+        }
+    return false;
+}
+
+if (IsPalindrome()) System.Console.WriteLine($"Введный текст/число - {number} - является палиндромом");
+else System.Console.WriteLine($"Число {number} не является палиндромом");
+System.Console.WriteLine();
