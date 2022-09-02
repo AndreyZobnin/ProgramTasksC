926be0d19682a625ec01a6ff88f160c37f803818
andrey <andreyz3000@ya.ru>
2022-09-02 20:34:48 +0300

    Переименовал в ДЗ 4 к семинару 2

--- Task4Seminar2/Program.cs
+++ Task4Seminar2/Program.cs
@@ -2,7 +2,7 @@
 int a = 7; // переменная а = 7
 int b = 23; 
 string Result = " "; //строковая переменная result
-if (number % a == 0 && number % b == 0); //Result = "кратно обоим числам"; // проверяем если при деление на число из random переменная a и переменная b равна 0
-else if (number % a != 0 && number % b != 0) //Result = "не кратно ни одному из чисел"; //если не равно
+if (number % a == 0 && number % b == 0) Result = "кратно обоим числам"; //Result = "кратно обоим числам"; // проверяем если при деление на число из random переменная a и переменная b равна 0
+else if (number % a != 0 && number % b != 0) Result = "некратно ни одному из чисел"; //Result = "не кратно ни одному из чисел"; //если не равно
 System.Console.WriteLine();
 System.Console.WriteLine($"Число кратное {number} {Result} "); //печатаем на экран
