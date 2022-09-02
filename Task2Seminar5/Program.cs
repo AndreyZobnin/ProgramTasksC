ad5cb243dd4f616e288a0ce8778d48d61d64f0c7
andrey <andreyz3000@ya.ru>
2022-09-02 20:57:27 +0300

    ДЗ 5 к семинару 2

---
+++ Task2Seminar5/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите количетво элементов в массиве" );
+int N = Convert.ToInt32(Console.ReadLine());
+int [] arr = new int [N];
+int countSum = 0;
+for (int i = 0; i < N; i++)
+{
+    arr[i] = new Random().Next (0, 100);
+    Console.Write(arr[i] + ", ");
+    if ((i % 2 != 0) && (arr[1] % 2 == 0))
+    {
+        countSum = countSum + arr[i];
+    }
+}
+
+Console.WriteLine("Сумма четных элементов на нечетных позициях " + countSum);
