dcf32d0b7e09d109e4212ce215cca58277c4eb44
andrey <andreyz3000@ya.ru>
2022-09-02 19:19:58 +0300

    Переименовал в ДЗ 1 к семинару 1

---
+++ Task3Seminar1/Program.cs
@@ -0,0 +1,12 @@
+﻿//3 дз
+int a;
+Console.Write("введите число: ");
+a =  Convert.ToInt32(Console.ReadLine());
+if (a % 2==1) //формула определение четности числа Если ... и т.д.
+    {
+    Console.WriteLine("число нечетное");
+    }
+    else
+    {
+    Console.WriteLine("Число четное");
+    }
