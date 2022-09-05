0f076be90c86de171ff2281e2325fb5916dfaf79
andrey <andreyz3000@ya.ru>
2022-09-05 15:26:28 +0300

    ДЗ 3  к семинару 5 доработал

--- Task3Seminar5/Program.cs
+++ Task3Seminar5/Program.cs
@@ -3,21 +3,25 @@ int N = Convert.ToInt32(Console.ReadLine()); // Конвертируем
 int [] massiv = new int [N]; // создаем массив количество берем из консоли N
 for (int i = 0; i < N; i++)
 {
-    Console.WriteLine("Введи индексы", + i + ":");
+    Console.WriteLine("Введи индексы c 0" );
     massiv[i] = Convert.ToInt32(Console.ReadLine());
+
     // massiv[i] = new Random().Next(N);
+//int max = Maximum(massiv);
+//int min = Minimum(massiv);
+//int tmp  = max - min;
 }
-int Minimum (int[] massiv) {
-    int minimum = massiv [0];
-    for (int j = 1; j < N; j++)
+int Minimum (int[] massiv) { //создаем метод который берет значения из массива
+    int minimum = massiv [0]; // создаем и переменная будет равна индексу массива
+    for (int j = 1; j < N; j++) //цикл итерация с J
     {
-        if (massiv[j] < minimum) {
-            minimum = massiv[j];
+        if (massiv[j] < minimum) { // условие сравниваем
+            minimum = massiv[j]; // вычисляем и записываем в переменную
         }
     }
-    return minimum;
+    return minimum; // возвращаем значение
 }
-int Maximum (int[] massiv) {
+int Maximum (int[] massiv) { // тоже самое только используем i и знак > больше
     int maximum = massiv [0];
     for (int i = 1; i < N; i++)
     {
@@ -27,7 +31,7 @@ int Maximum (int[] massiv) {
     }
     return maximum;
 }
-int maximum = Maximum(massiv);
-int minimum = Minimum(massiv);
-int tmp  = maximum - minimum;
-Console.WriteLine("разница " + tmp );
+int max = Maximum(massiv);
+int min = Minimum(massiv);
+int tmp  = max - min;
+Console.WriteLine("разница "  + tmp );
